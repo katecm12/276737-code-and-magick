35127d05d2b5aa2c35ade871698de962f4aead4e
blnknj <katerina.cm12@mail.ru>
2017-03-25 15:57:17 +0300

    Revert "attempt_1"
    This reverts commit 0ffdc505d799276273e5862e3413029bc54f05c0.

--- js/game.js
+++ js/game.js
@@ -1,26 +1,23 @@
 'use strict';
-var fireballSize;
-var getFireballSpeed;
-var getWizardHeight;
 
 window.GameConstants = {
   Fireball: {
-    size: window.fireballSize || 22,
+    size: window.fireballSize || 24,
     speed: window.getFireballSpeed || function (movingLeft) {
       return movingLeft ? 2 : 5;
     }
   },
   Wizard: {
-    speed: window.wizardSpeed || 3,
-    width: window.wizardWidth || 70,
+    speed: window.wizardSpeed || 2,
+    width: window.wizardWidth || 61,
     getHeight: window.getWizardHeight || function (width) {
       return 1.377 * width;
     },
     getX: window.getWizardX || function (width) {
-      return width / 2;
+      return width / 3;
     },
     getY: window.getWizardY || function (height) {
-      return 2 * height / 3;
+      return height - 100;
     }
   }
 };
