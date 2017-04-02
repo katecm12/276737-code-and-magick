e4790057910870ea3fc8205f8816946d8164e292
blnknj <katerina.cm12@mail.ru>
2017-04-02 18:30:46 +0300

    12

--- js/stat.js
+++ js/stat.js
@@ -11,14 +11,44 @@
         ctx.font = '16px PT Mono';
         ctx.fillText('Ура, вы победили!', 120, 40);
         ctx.fillText('Список результатов:', 120, 60);
-        var max = -1;
-        var maxIndex = -1;
-        for (var i = 0; i < times.length; i++) {
-        var time = times[i];
-        if (time > max) {
+    
+  var max = -1;
+  var maxIndex = -1;
+
+  for (var i = 0 ; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
       max = time;
-      maxIndex = i
-  }    
+      maxIndex = i;
     }
-    };
+  }
+ 
+  function round(time) {
+    return Math.round(time); // попытка округлить время
+};
+        
+  var histogramHeight = 150;              // px;
+  var step = histogramHeight / (max - 0); // px;
+  
+  var barWidth = 40; // px; 
+  var indent = 90;    // px;
+  var initialX = 140; // px;
+  var initialY = 80;  // px;
+        
+  for(var i = 0; i < times.length; i++) {
+    ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * step);
+    ctx.fillText(names[i], 140 + indent * i, 250);    
+    ctx.fillText(times[i], 140 + indent * i, times[i] * step); 
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)', 'rgba(0, 0, 0, 0.5)';
+    }   
+    };    
+ 
 }());
+
+// ctx.fillRect(10, 10, 55, 50);   
+// draws the rectangle at position 10, 10 with a width of 55 and a height of 50
+// Высота гистограммы 150px.
+
+// Ширина колонки 40px.
+
+// Расстояние между колонками 50px.
